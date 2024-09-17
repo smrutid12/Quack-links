b43a884b7f2a2835eb787685622ac2dd0a88e096
Smruti Dash <smrutid12@gmail.com>
2024-09-17 19:59:18 +0400

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,9 +1,8 @@
-import os
 import axios from "axios";
 
 // Set up base URL for your Flask backend
 const api = axios.create({
-  baseURL: os.getenv('BACKEND_URL'), // Update with your Flask server URL
+  baseURL: process.env.REACT_APP_BACKEND_URL, // Use environment variable for Flask server URL
   headers: {
     "Content-Type": "application/json",
   },
