76ca4b6029b7c6be179f032ce850a2122ad5b12e
Smrutiworkz12 <smruti.dash@workz.com>
2026-09-02 13:47:13 +0400

    fix: updated the REACT_APP_API_BASE_URL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-console.log(
-  "API URL:",
-  process.env.NEXT_PUBLIC_BACKEND_URL
-);
-
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
